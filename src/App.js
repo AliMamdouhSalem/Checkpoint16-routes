aead66f30a5992adaac664e686109a008d613111
Ali <alimamdouhsalem@gmail.com>
2021-09-28 13:09:48 +0200

    checkpoint15New

--- src/App.js
+++ src/App.js
@@ -1,69 +1,270 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
-import { NavDropdown } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieList from './components/MovieList';
-import React,{ useState } from "react"; 
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from "./components/MovieList";
+import React, { useState } from "react";
 function App() {
-  const[movies, setMovies]= useState([
-    {title: "The shawshank redemption", description:"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency. Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit.", posterURL:"https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg", rating:'9.3', id:1 },
-    {title: "eternal sunshine of the spotless mind", description:"Eternal Sunshine of the Spotless Mind (also simply known as Eternal Sunshine) is a 2004 American science fiction romantic comedy-drama film written by Charlie Kaufman and directed by Michel Gondry. It follows an estranged couple who have erased each other from their memories", posterURL:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRlDad1DnSApJJ9OHQM9IazQ_goiHSRYibeIYk0xF5cURWL9vlm", rating:'8.3', id:2 }
+  const [movies, setMovies] = useState([
+    {
+      title: "The shawshank redemption",
+      description:
+        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency. Chronicles the experiences of a formerly successful banker as a prisoner in the gloomy jailhouse of Shawshank after being found guilty of a crime he did not commit.",
+      posterURL:
+        "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
+      rating: "9",
+      id: 1,
+    },
+    {
+      title: "eternal sunshine of the spotless mind",
+      description:
+        "Eternal Sunshine of the Spotless Mind (also simply known as Eternal Sunshine) is a 2004 American science fiction romantic comedy-drama film written by Charlie Kaufman and directed by Michel Gondry. It follows an estranged couple who have erased each other from their memories",
+      posterURL:
+        "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRlDad1DnSApJJ9OHQM9IazQ_goiHSRYibeIYk0xF5cURWL9vlm",
+      rating: "6",
+      id: 2,
+    },
+  ]);
 
-])
-  const handleAdd =() => {
-    const tiltex= prompt("please enter title");
-    const descriptionx= prompt("please enter description");
-    const url= prompt("please enter poster URL");
-    const ratings= prompt("please enter rating");
-    const idx= (movies.length+1);
-    console.log(movies);
-    setMovies(movies=>[...movies, {title:tiltex, description:descriptionx, posterURL:url, rating:ratings, id:idx }]);
-  }
-  const handleFilterRating = () =>{
-    const lower= parseFloat(prompt("please enter rating lower limit(if you want one rating enter it in both prompts)"),10);
-    const higher= parseInt(prompt("please enter rating higher limit"),10);
-    if(lower==higher){
-      const newMovies= movies.filter(rating=> rating==higher);
-      setMovies(newMovies);
-    }
-    else if (higher<lower){
+  const [addForm, setAddForm] = useState({
+    title: "",
+    description: "",
+    url: "",
+    rate: "",
+    id: 0,
+  });
+  const [minRating, setMinRating] = useState(0);
+  const [maxRating, setMaxRating] = useState(0);
+  const [title, setTitle] = useState("");
+  const [showAdd, setShowAdd] = useState(false);
+  const [showFilterName, setShowFilterName] = useState(false);
+  const [showFilterRate, setShowFilterRate] = useState(false);
+  const handleAddFormOpen = () => setShowAdd(true);
+  const handleAddFormClose = () => setShowAdd(false);
+  const handleFilterRatingFormOpen = () => setShowFilterRate(true);
+  const handleFilterRatingFormClose = () => setShowFilterRate(false);
+  const handleFilterNameFormOpen = () => setShowFilterName(true);
+  const handleFilterNameFormClose = () => setShowFilterName(false);
+  const handleAdd = () => {
+    setAddForm({ ...addForm, id: movies.length + 1 });
+    setMovies((movies) => [...movies, addForm]);
+  };
+  const handleFilterRating = () => {
+    console.log(minRating, maxRating);
+    if (maxRating < minRating) {
       alert("Please enter right data!!");
     }
-    else{
-      
-      const newMovies= movies.filter(movie=>(movie.rating>lower && movie.rating<higher ));
-      console.log(newMovies)
+    if (minRating === maxRating) {
+      const newMovies = movies.filter((rating) => rating === maxRating);
+      console.log(newMovies);
       setMovies(newMovies);
     }
-    
-  }
-  const handleFilterName = () =>{
-    const name= prompt("please enter required name or key word")
-    const newMovies= movies.filter(movie => movie.title.includes(name))
+
+    const newMovies = movies.filter(
+      (movie) => movie.rating > minRating && movie.rating < maxRating
+    );
+    console.log(newMovies);
     setMovies(newMovies);
-    
-  }
+  };
+  const handleFilterName = () => {
+    const newMovies = movies.filter((movie) => movie.title.includes(title));
+    setMovies(newMovies);
+  };
   return (
-    
     <div className="App">
-    <div className="content">
-    <Navbar sticky="top" bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Best movies</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home" onClick={() => handleAdd()}>Add movie</Nav.Link>
-      <Nav.Link href="#home" onClick={() => handleFilterRating()}>Filter by rating</Nav.Link>
-      <Nav.Link href="#home" onClick={() => handleFilterName()}>Filter by name</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-    </div>
-    <div>
-      <MovieList movies={movies}/>
-    </div>
+      <div className="content">
+        <Navbar sticky="top" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Best movies</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home" onClick={() => handleAddFormOpen()}>
+                Add movie
+              </Nav.Link>
+              <Nav.Link
+                href="#home"
+                onClick={() => handleFilterRatingFormOpen()}
+              >
+                Filter by rating
+              </Nav.Link>
+              <Nav.Link href="#home" onClick={() => handleFilterNameFormOpen()}>
+                Filter by name
+              </Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        {/* Popup Add Movie */}
+        <Modal
+          show={showAdd}
+          onHide={handleAddFormClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Add Movie</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <Form.Group className="mb-3" controlId="movieTitle">
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter title"
+                  value={addForm.title}
+                  onChange={(e) =>
+                    setAddForm({ ...addForm, title: e.target.value })
+                  }
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="movieDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  placeholder="Enter description"
+                  value={addForm.description}
+                  onChange={(e) =>
+                    setAddForm({ ...addForm, description: e.target.value })
+                  }
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="moviePoster">
+                <Form.Label>Poster URL</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter poster URL"
+                  value={addForm.url}
+                  onChange={(e) =>
+                    setAddForm({ ...addForm, url: e.target.value })
+                  }
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="movieRating">
+                <Row>
+                  <Col>
+                    <Form.Label>Rating</Form.Label>
+                    <Form.Control
+                      type="number"
+                      placeholder="Enter rate"
+                      value={addForm.rate}
+                      onChange={(e) =>
+                        setAddForm({ ...addForm, rate: e.target.value })
+                      }
+                    />
+                  </Col>
+                  <Col className="mt-3 pt-3">
+                    <h2>/10</h2>
+                  </Col>
+                </Row>
+              </Form.Group>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleAddFormClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleAdd}>
+              Add
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* Popup Filter Rating */}
+        <Modal
+          show={showFilterRate}
+          onHide={handleFilterRatingFormClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Filter by rate</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <Form.Group className="mb-3" controlId="minRating">
+                <Row>
+                  <Col>
+                    <Form.Label>From</Form.Label>
+                    <Form.Control
+                      type="number"
+                      placeholder="Enter starting rate"
+                      value={minRating}
+                      onChange={(e) => setMinRating(+e.target.value)}
+                    />
+                  </Col>
+                  <Col className="mt-4 pt-2">
+                    <h2>/10</h2>
+                  </Col>
+                </Row>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="maxRating">
+                <Row>
+                  <Col>
+                    <Form.Label>To</Form.Label>
+                    <Form.Control
+                      type="number"
+                      placeholder="Enter max rate"
+                      value={maxRating}
+                      onChange={(e) => setMaxRating(+e.target.value)}
+                    />
+                  </Col>
+                  <Col className="mt-4 pt-2">
+                    <h2>/10</h2>
+                  </Col>
+                </Row>
+              </Form.Group>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleFilterRatingFormClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleFilterRating}>
+              Filter
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {/* Popup Filter Name */}
+        <Modal
+          show={showFilterName}
+          onHide={handleFilterNameFormClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Filter by Name</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form>
+              <Form.Group>
+                <Form.Label>Search</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter search term"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </Form.Group>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleFilterNameFormClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleFilterName}>
+              Filter
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      <div>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
---
+++ src/components/AddNewMovie.js
@@ -0,0 +1,11 @@
+import { queryByDisplayValue } from '@testing-library/react';
+import React from 'react';
+const AddNewMovie = ({handleClose, show}) => {
+    return ( 
+    <div>
+
+    </div>
+    );
+}
+ 
+export default AddNewMovie;
