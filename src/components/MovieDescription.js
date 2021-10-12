1f9c1758881046b11f19e1c221b02e072dd2c956
Ali <alimamdouhsalem@gmail.com>
2021-10-12 05:16:45 +0200

    Checkpoint16

--- src/components/MovieDescription.js
+++ src/components/MovieDescription.js
@@ -1,12 +1,12 @@
 import { useParams } from "react-router";
 
 const MovieDescription = () => {
-    var {name, desc,trailerLink,movies} = useParams()
+    var {name, desc,trailerLink} = useParams()
     console.log(trailerLink);
     console.log("heyy");
-    console.log(movies);
-    var selectTrailer= movies.filter((movies)=>movies.title==name);
-    const trailer= selectTrailer.trailerURL
+    // console.log(movies);
+    // var selectTrailer= movies.filter((movies)=>movies.title==name);
+    // const trailer= selectTrailer.trailerURL
     return (
         <div>
             <h1>HIIIII</h1>
